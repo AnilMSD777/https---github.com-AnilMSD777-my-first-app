129ceca256e71f0ce54bc247157d336bcf2f5edd
Anil <anilkumarkota1998@gmail.com>
2024-03-12 23:33:24 +0530

    Ways to use interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,8 @@ import { Component } from "@angular/core";
     templateUrl : './server.component.html'
 })
 export class ServerComponent{
-
+name:string = 'myServer';
+getName(){
+    return 'this is from getName method';
+}
 }
