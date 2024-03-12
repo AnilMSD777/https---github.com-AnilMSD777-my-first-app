5ed30775b4763f34f952975c9eb87ff9db1416e0
Anil <anilkumarkota1998@gmail.com>
2024-03-12 23:57:05 +0530

    2 way data binding on input text field

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,9 +5,12 @@ import { Component } from "@angular/core";
     templateUrl : './server.component.html'
 })
 export class ServerComponent{
-buttonText:string = 'Button was not clicked';
+    inputText:string = 'initial value';
+    inputText2:string = 'initial value 2';
 ngOnInit(){
-    
+}
+onInputChange(event:any){
+    this.inputText = event.target.value;
 }
 getName(){
     return 'this is from getName method';
